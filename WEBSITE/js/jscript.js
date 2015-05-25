fad1b3270f93c5ee1d74f556e9c293c5a897068c
Lorenzo Rizzi <lorenzo.rzz.92@gmail.com>
2015-05-25 21:55:57 +0200

    aggiunta carousel per la gallery dei nostri spazi

--- WEBSITE/js/jscript.js
+++ WEBSITE/js/jscript.js
@@ -3,7 +3,31 @@ var GIndex=1;
 $(document).ready(Ready);
 
 function Ready(){
-    setInterval(animateGallery, 2000);
+//    setInterval(animateGallery, 2000);
+     // Attivo Carousel
+    $("#myCarousel").carousel({interval: 500});
+    
+    // abilito gli indicatori di Carousel
+    $(".item1").click(function(){
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+        $("#myCarousel").carousel(3);
+    });
+    
+    // abilito controlli Carousel 
+    $(".left").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+        $("#myCarousel").carousel("next");
+    });
 }
 
 function animateGallery(){
