9882948b6d71f82d15dbcc7c959d9cf6d0bc8850
Lorenzo Rizzi <lorenzo.rzz.92@gmail.com>
2015-05-28 20:54:02 +0200

    inserite immagini dei corsi da db

--- WEBSITE/js/specificCourse.js
+++ WEBSITE/js/specificCourse.js
@@ -36,6 +36,11 @@ function Ready(){
             success: function(response){
                 var courseItem=JSON.parse(response);
                 $("#content").append("<h2>"+courseItem[0].nome+"</h2>");
+                $("#content").append("<img src=\""+courseItem[0].img1+"\">");
+                $("#content").append("<img src=\""+courseItem[0].img2+"\">");
+                if(courseItem[0].img3 != null){
+                    $("#content").append("<img src=\""+courseItem[0].img3+"\">");
+                }
                 $("#content").append("<div>"+courseItem[0].descrizione+"</div>");
                 $("#content").append("<div><b>Categoria:</b>"+courseItem[0].nomeCat+"</div>");
                 
